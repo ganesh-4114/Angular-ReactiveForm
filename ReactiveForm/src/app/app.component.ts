0c43ee4a4e60b5901fe59324bfbd4075ff22ce25
ganesh-4114 <ganeshdandamudi16@gmail.com>
2022-04-22 18:27:02 +0530

    added bootstrap

--- ReactiveForm/src/app/app.component.ts
+++ ReactiveForm/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ReactiveForm';
+
+  objectKeys = Object.keys;
+
+  countryList = {1: 'India', 2: 'Sri Lanka', 3: 'New Zealand'};
+
+  // countryList = ['India', 'America'];
+  contactForm = new FormGroup({
+    firstname: new FormControl(),
+    lastname: new FormControl(),
+    email: new FormControl(),
+    gender: new FormControl(),
+    isMarried: new FormControl(),
+    country: new FormControl()
+  })
+
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
+
 }
